ded36907626a17328f71f7da8114bb5fbb8bdf7d
Adrian Cuyugan <adrian@foxyreign.com>
2014-05-08 14:18:09 -0500

    Plot 1

--- plot1.R
+++ plot1.R
@@ -10,6 +10,5 @@ feb <- subset(data, Date == '1/2/2007' | Date == '2/2/2007')
 # Plot histogram
 par(mfrow=c(1,1))
 png(filename='plot1.png', width=480, height=480)
-hist(as.numeric(feb$Global_active_power), xlab='Global Active Power (kilowatts)', 
-     main='Global Active Power', col='red')
+hist(as.numeric(feb$Global_active_power), xlab='Global Active Power (kilowatts)', main='Global Active Power', col='red')
 dev.off()
