9d96d21c1771796db57887dae1720e6754aa4349
Adrian Cuyugan <adrian@foxyreign.com>
2014-05-08 14:22:25 -0500

    Plot 4. Legend needs rework on PNG

---
+++ plot4.R
@@ -0,0 +1,32 @@
+# Load libraries
+library(data.table)
+
+# Load whole data into workspace using fread
+data <- fread('household_power_consumption.txt', header=T, sep=';')
+
+# Subset only Feb 1 and 2
+feb <- subset(data, Date == '1/2/2007' | Date == '2/2/2007')
+
+# Convert Date and Time
+feb$DateTime <- strptime(paste(feb$Date, feb$Time), format="%d/%m/%Y %H:%M:%S")
+
+# Plot histogram
+par(mfrow=c(2,2))
+with(feb, {
+    # Plot 1
+    plot(x=feb$DateTime, y=as.numeric(feb$Global_active_power), type='l', xlab='', ylab='Global Active Power')
+    
+    # Plot 2
+    plot(x=feb$DateTime, y=as.numeric(feb$Voltage), type='l', xlab='datetime', ylab='Voltage')
+    
+    # Plot 3
+    plot(x=feb$DateTime, y=as.numeric(feb$Sub_metering_1), type='l', xlab='', ylab='Energy sub metering')
+    lines(x=feb$DateTime, y=as.numeric(feb$Sub_metering_2), type='l', col='red')
+    lines(x=feb$DateTime, y=as.numeric(feb$Sub_metering_3), type='l', col='blue')
+    legend(x='topright', legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), col=c('black', 'red', 'blue'), lty=c(1, 1, 1), cex=0.6, bty='n')
+    
+    # Plot 4
+    plot(x=feb$DateTime, y=as.numeric(feb$Global_reactive_power), type='l', xlab='datetime', ylab='Global_reactive_power')
+})
+dev.copy(png, file='plot4.png', width=480, height=480)
+dev.off()
