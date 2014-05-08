74b03e4453669ef41699e759205cf30a24f237e2
Adrian Cuyugan <adrian@foxyreign.com>
2014-05-08 14:42:26 -0500

    Plot 1. Changed background to transparent.

--- plot1.R
+++ plot1.R
@@ -9,6 +9,6 @@ feb <- subset(data, Date == '1/2/2007' | Date == '2/2/2007')
 
 # Plot histogram
 par(mfrow=c(1,1))
-png(filename='plot1.png', width=480, height=480)
+png(filename='plot1.png', width=480, height=480, bg='transparent')
 hist(as.numeric(feb$Global_active_power), xlab='Global Active Power (kilowatts)', main='Global Active Power', col='red')
 dev.off()
